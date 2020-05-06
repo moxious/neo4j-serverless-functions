0193c79e1ac1cc3f0bd5c68b32f26af732562893
Moxious <moxious@oldhat.org>
2020-05-06 10:31:38 -0400

    sample CUD generation

---
+++ cud/generateCUD.js
@@ -0,0 +1,20 @@
+const uuid = require('uuid');
+const test = require('../test/index');
+
+const buf = [];
+for (let i=0; i<100; i++) {
+    const msg = {
+        op: 'create',
+        type: 'node',
+        labels: ['Test'],
+        properties: {
+            index: i,
+            x: Math.random(),
+            uuid: uuid.v4(),
+        },
+    };
+
+    buf.push(test.generateCUDMessage());
+}
+
+console.log(JSON.stringify(buf));
--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
 const sinon = require('sinon');
+const uuid = require('uuid');
+const moment = require('moment');
 
 module.exports = {
     mockResponse: () => {
@@ -12,5 +14,36 @@ module.exports = {
         };
 
         return res;
+    },
+
+    generateCUDMessage: (op='create', type='node') => {
+        const labels = ['Person', 'Job', 'Company'];
+        const msg = { op, type };
+
+        const someProps = () => ({
+            id: Math.floor(Math.random() * 100000),
+            uuid: uuid.v4(),
+            x: Math.random(),
+            when: moment.utc().toISOString(),
+        });
+
+        const someKeys = () => ({ id: Math.floor(Math.random() * 100000) });
+        const randLabels = () => [labels[Math.floor(Math.random()*labels.length)]];
+
+        if (type === 'relationship') {
+            msg.from = {
+                labels: randLabels(),
+            };
+
+            msg.to = {
+                labels: randLabels(),
+            };
+        } else {
+            msg.labels = randLabels();
+            msg.ids = someKeys();
+        }
+
+        msg.properties = someProps();
+        return msg;
     },
 };
