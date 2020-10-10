6db6dbeac07366fdc58961d2f7e2ef270bebbc81
Moxious <moxious@oldhat.org>
2020-10-10 10:42:03 -0400

    accept env vars in addition to secrets manager

--- gil/driver/driverConfiguration.js
+++ gil/driver/driverConfiguration.js
@@ -5,29 +5,43 @@ const project = process.env.GOOGLE_PROJECT || 'graphs-are-everywhere';
 
 // For proper auth, set GOOGLE_APPLICATION_CREDENTIALS to the key
 // that contains project information & service account.
-const getDriverOptions = async () => {
-    const client = new SecretManagerServiceClient();
-
+const getDriverOptions = async () => {    
     const DRIVER_OPTIONS = {
         maxConnectionLifetime: 8 * 1000 * 60, // 8 minutes
         connectionLivenessCheckTimeout: 2 * 1000 * 60,
     };
 
-    const [uriResponse] = await client.accessSecretVersion({
-        name: `projects/${project}/secrets/NEO4J_URI/versions/latest`,
-    });
+    let uri, user, password;
 
-    const [userResponse] = await client.accessSecretVersion({
-        name: `projects/${project}/secrets/NEO4J_USER/versions/latest`,
-    });
+    try {
+        const client = new SecretManagerServiceClient();
+        const [uriResponse] = await client.accessSecretVersion({
+            name: `projects/${project}/secrets/NEO4J_URI/versions/latest`,
+        });
 
-    const [passwordResponse] = await client.accessSecretVersion({
-        name: `projects/${project}/secrets/NEO4J_PASSWORD/versions/latest`,
-    });
+        const [userResponse] = await client.accessSecretVersion({
+            name: `projects/${project}/secrets/NEO4J_USER/versions/latest`,
+        });
 
-    const uri = uriResponse.payload.data.toString('utf8');
-    const user = userResponse.payload.data.toString('utf8');
-    const password = passwordResponse.payload.data.toString('utf8');
+        const [passwordResponse] = await client.accessSecretVersion({
+            name: `projects/${project}/secrets/NEO4J_PASSWORD/versions/latest`,
+        });
+
+        uri = uriResponse.payload.data.toString('utf8');
+        user = userResponse.payload.data.toString('utf8');
+        password = passwordResponse.payload.data.toString('utf8');
+    } catch (err) {
+        console.error('Failed to use Google Secrets Manager to configure the environment, due to:');
+        console.error(err);
+
+        uri = process.env.NEO4J_URI;
+        user = process.env.NEO4J_USER;
+        password = process.env.NEO4J_PASSWORD;
+    }
+
+    if (!uri || !user || !password) {
+        throw new Error('You must either configure Google Secrets Manager for connection details, or you must specify environment variables');
+    }
 
     const auth = neo4j.auth.basic(user, password);
 
